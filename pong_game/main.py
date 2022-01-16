470bc710ffd79cf779c364b5d7bc608f51bed02d
jongseok <jjs0615@naver.com>
2022-01-16 21:56:13 +0900

    The start of the pong game.

---
+++ pong_game/main.py
@@ -0,0 +1,7 @@
+
+##### create the screen
+##### makes two bar with 4 blocks
+##### draws a line in the middle of center
+##### two scoreboards
+##### needs a moving ball
+##### detect when paddle misses
