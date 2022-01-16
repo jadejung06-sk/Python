f300b7d29920b56a42bea33400d02716678705ae
jongseok <jjs0615@naver.com>
2022-01-16 19:26:56 +0900

    Make a Scoreboard

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,9 @@
 from snake import Snake
 from turtle import Screen, Turtle
+from food import Food
+from scoreboard import Scoreboard
 import time
+
 
 screen = Screen()
 screen.setup(width = 600, height = 600) # ★
@@ -8,8 +11,10 @@ screen.bgcolor('black')
 screen.title("My Snake Game")
 screen.tracer(0) # ★
 
-
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 ##### control the direction
 screen.listen() 
 screen.onkey(snake.up,"Up") # ★
@@ -17,11 +22,15 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_is_on = True
 while game_is_on:
     screen.update() # ★
     time.sleep(0.1)
     snake.move()
+
+    ##### detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.get_score()
 
 screen.exitonclick() # ★
---
+++ snake_game/scoreboard.py
@@ -0,0 +1,23 @@
+from re import L
+from turtle import Turtle
+ALIGNMENT = 'center'
+FONT = ("Arial", 12, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0,280)
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score :  {self.score}", move = False, align= ALIGNMENT, font = FONT)
+        
+    def get_score(self):
+        self.score += 1
+        self.clear() # ★
+        self.update_score()
