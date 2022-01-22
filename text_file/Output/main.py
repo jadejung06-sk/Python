7ce3fa4417dfbfa499440d0cf995ae267370d600
jongseok <jjs0615@naver.com>
2022-01-22 18:53:26 +0900

    mailing system done

--- text_file/Output/main.py
+++ text_file/Output/main.py
@@ -1,5 +1,7 @@
 from distutils import text_file
 
+PLACEHOLDER = '[name]'
+
 with open('./text_file/Input/Letters/starting_letter.txt') as letter:
     contents = letter.read()
 
@@ -7,10 +9,9 @@ with open('./text_file/Input/Names/invited_names.txt') as names:
     name = names.read().split('\n')
     names_list = []
     names_list.append(name)
-    # print(names_list[0])
     names.close()
 
 for invited_person in names_list[0]:
     with open(f'./text_file/Output/ReadyToSend/to_{invited_person}.txt', mode = 'w') as new_letter:
-        new_letter.write(contents.replace('[name]', invited_person))
+        new_letter.write(contents.replace(PLACEHOLDER, invited_person)) # ★
 
