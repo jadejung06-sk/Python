c01634a66ab4654cb9f94323ce1b517e9ced14b2
jongseok <jjs0615@naver.com>
2022-01-22 22:15:43 +0900

    learn_pandas

--- csv_file/main.py
+++ csv_file/main.py
@@ -1,20 +1,9 @@
-PATH = './csv_file/weather_data.csv'
-### method 1
-# with open(PATH) as data_file:
-#     data = data_file.readlines()
-#     print(data)
-
-### method 2
-# import csv
-# with open(PATH) as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != 'temp': # ★
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-
-### method 3
 import pandas as pd
-data = pd.read_csv(PATH)
-print(data["temp"])
+PATH = "./csv_file/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv"
+
+squirrel_data = pd.read_csv(PATH)
+print(squirrel_data.columns)
+print(squirrel_data["Primary Fur Color"])
+print(squirrel_data['Highlight Fur Color'])
+print(squirrel_data['Combination of Primary and Highlight Color'])
+# print(squirrel_data['Color notes'])
