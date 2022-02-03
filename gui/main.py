ca9089fde16da46dbb88e8ce62ce18aa0fa9e4fe
jongseok <jjs0615@naver.com>
2022-02-03 23:07:45 +0900

    TK_get coding

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,6 @@
 import tkinter
+from tkinter.ttk import Button
+from webbrowser import get
 
 # https://docs.python.org/3/library/tkinter.html#the-packer
 # http://tcl.tk/man/tcl8.6/TkCmd/pack.htm
@@ -9,14 +11,21 @@ window.minsize(width= 500, height = 300)
 
 #Label
 my_label = tkinter.Label(text = "I AM a Label", font = ("Arial", 24, "italic"))
-my_label.pack(side = 'left')
+my_label.pack()
+my_label['text'] = 'New Text'
+my_label.config(text = "New Text")
 
+# Button
+def button_clicked():
+    my_label.config(text = "got clicked")
 
+button = Button(text = "Click Me", command= button_clicked) # ★
+button.pack()
 
-
-
-
-
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
+print(input.get()) # ★
 
 
 
