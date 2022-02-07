0dd42289fb834f3b1022f1a89a9050f1a78158bd
jongseok <jjs0615@naver.com>
2022-02-08 07:35:57 +0900

    step to after func.

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -23,6 +23,8 @@ window.title("Pomodoro")
 window.minsize(width=400, height = 400)
 window.config(padx=100, pady=50, bg = YELLOW) # ★
 
+# window.after()
+
 ### label
 break_label = tk.Label(text = 'Timer', bg = YELLOW, fg = PINK,  font = (FONT_NAME, 35, 'bold'))
 break_label.grid(column = 1, row = 0 )
@@ -30,12 +32,12 @@ break_label.grid(column = 1, row = 0 )
 ## buttons
 def start_btn_clicked():
     print("start_btn")
-start_btn = tk.Button(text= 'Start', command = start_btn_clicked)
+start_btn = tk.Button(text= 'Start', command = start_btn_clicked, highlightthickness=0)
 start_btn.grid(column= 0, row = 2)
 
 def start_btn_clicked():
     print("start_btn")
-reset_btn = tk.Button(text= 'Reset', command = start_btn_clicked)
+reset_btn = tk.Button(text= 'Reset', command = start_btn_clicked, highlightthickness=0)
 reset_btn.grid(column= 2, row = 2)
 
 ### tomato timer
@@ -47,8 +49,8 @@ canvas.create_text(100, 130, text = f'00:00', fill = "white", font = (FONT_NAME,
 canvas.grid(column=1, row=1)
 
 ### check mark
-check_mark = "✔"
-check_label = tk.Label(text = check_mark, bg = YELLOW, fg = GREEN,  font = (FONT_NAME, 15, 'bold'))
+check_marks = "✔"
+check_label = tk.Label(text = check_marks, bg = YELLOW, fg = GREEN,  font = (FONT_NAME, 15, 'bold'))
 check_label.grid(column = 1, row = 3 )
 
 
