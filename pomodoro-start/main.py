a798769c401a777ebb6908d2b55cad4f2d5a8d67
jongseok <jjs0615@naver.com>
2022-02-07 22:54:03 +0900

    added tomato images

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -21,10 +21,10 @@ LONG_BREAK_MIN = 20
 window = tk.Tk()
 window.title("Pomodoro")
 window.minsize(width=400, height = 400)
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg = YELLOW) # ★
 
 ### label
-break_label = tk.Label(text = 'Break', fg = PINK, font=20)
+break_label = tk.Label(text = 'Timer', bg = YELLOW, fg = PINK,  font = (FONT_NAME, 35, 'bold'))
 break_label.grid(column = 1, row = 0 )
 
 ## buttons
@@ -40,10 +40,16 @@ reset_btn.grid(column= 2, row = 2)
 
 ### tomato timer
 file = './pomodoro-start/tomato.png'
-canvas = tk.Canvas(width=200, height= 224)
+canvas = tk.Canvas(width=200, height= 224, bg = YELLOW, highlightthickness= 0) # ★
 tomato_img = tk.PhotoImage(file = file)
-canvas.create_image(103, 112, image = tomato_img)
-canvas.create_text(103, 112, text = f'00:00')
+canvas.create_image(100, 112, image = tomato_img)
+canvas.create_text(100, 130, text = f'00:00', fill = "white", font = (FONT_NAME, 35, 'bold')) # ★
 canvas.grid(column=1, row=1)
+
+### check mark
+check_mark = "✔"
+check_label = tk.Label(text = check_mark, bg = YELLOW, fg = GREEN,  font = (FONT_NAME, 15, 'bold'))
+check_label.grid(column = 1, row = 3 )
+
 
 window.mainloop()
