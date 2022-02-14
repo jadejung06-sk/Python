b26cd284f943a99337e8f86c84d95b319336cf95
jongseok <jjs0615@naver.com>
2022-02-15 07:39:57 +0900

    save func coding

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,15 +1,20 @@
 import tkinter as tk
-from turtle import width
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    path = './password-manager-start/data.txt'
+    with open(path, 'r') as id:
+        text = id.read()
+        print(text) ### ing
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title("Password Manager")
-window.geometry('600x500')
-window.config(padx=20, pady=20)
+# window.geometry('600x500')
+window.config(padx=50, pady=50)
 file = './password-manager-start/logo.png'
 canvas = tk.Canvas(width=200, height= 200)
 logo_img = tk.PhotoImage(file = file) # ★
@@ -18,20 +23,22 @@ canvas.grid(column=1, row=0)
 
 web_label = tk.Label(text = "Website: ")
 web_list = tk.Entry(width = 35)
+web_list.focus()
 web_list.grid(column=1, row =1, columnspan=2) # ★
 web_label.grid(column=0, row=1)
 
 email_label = tk.Label(text = "Email/Username: ")
-email_list = tk.Entry( width=35)
+email_list = tk.Entry(width=35)
+email_list.insert(0, "abc@naver.com")
 email_list.grid(column=1, row= 2, columnspan=2)
 email_label.grid(column=0, row=2)
 
 pw_label = tk.Label(text = "Password: ")
-pw_list = tk.Entry(width=21)
+pw_list = tk.Entry(width = 19)
 pw_list.grid(column=1, row=3)
 pw_label.grid(column=0, row=3)
 
-add_btn = tk.Button(text="Add", width = 36)
+add_btn = tk.Button(text="Add", width = 35, command=save)
 add_btn.grid(column=1, row=4, columnspan=2)
 gen_pw_btn = tk.Button(text = "Generate Password")
 gen_pw_btn.grid(column=2, row=3)
