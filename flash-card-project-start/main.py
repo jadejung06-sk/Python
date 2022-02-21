59b2cc7a3354a46b9d3e5dce8adc913f0cf02f7d
jongseok <jjs0615@naver.com>
2022-02-22 07:44:47 +0900

    flash card start

---
+++ flash-card-project-start/main.py
@@ -0,0 +1,15 @@
+BACKGROUND_COLOR = "#B1DDC6"
+CARD_BACK = "./flash-card-project-start/images/card_back.png"
+CARD_FRONT = "./flash-card-project-start/images/card_front.png"
+RIGHT = "./flash-card-project-start/images/right.png"
+WRONG = "./flash-card-project-start/images/wrong.png"
+WORDS = "./flash-card-project-start/data/french_words.csv"
+import tkinter as tkt
+
+window = tkt.Tk()
+
+
+tkt.Image()
+
+
+window.mainloop()
