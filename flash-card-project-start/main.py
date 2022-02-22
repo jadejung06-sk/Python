e4ba648a7f467c0f615a0507cdf9ee5cb949050d
jongseok <jjs0615@naver.com>
2022-02-23 07:42:10 +0900

    read cards

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,5 +1,6 @@
 import tkinter as tkt
 import pandas as pd
+import random
 BACKGROUND_COLOR = "#B1DDC6"
 window = tkt.Tk()
 window.title("Flashy")
@@ -14,22 +15,35 @@ WRONG = tkt.PhotoImage(file = "./flash-card-project-start/images/wrong.png")
 WORDS =  pd.read_csv("./flash-card-project-start/data/french_words.csv")
 canvas = tkt.Canvas(window, width = WIDTH, height= HEIGHT, bg=BACKGROUND_COLOR, highlightthickness=0) # ★
 canvas.create_image(WIDTH/2,HEIGHT/2, image = CARD_FRONT)
-canvas.create_text(400,150, text = f'Title', font=("Ariel", 40, "italic"))
-canvas.create_text(400,260, text = f'word', font=("Ariel", 60, "bold"))
 # canvas.create_image(550,720, image = CORRECT)
 # canvas.create_image(250,720, image = WRONG)
 
 ### labels
-title_label = tkt.Label(canvas, text = f"title")
+## words method 1
+# num = random.randint(0,WORDS.shape[0])
+# french_word = WORDS.French
+# canvas.create_text(400,150, text = f'{french_word.name}', font=("Ariel", 40, "italic"))
+# canvas.create_text(400,260, text = f'{french_word[num]}', font=("Ariel", 60, "bold"))
+
+## words method 2
+def next_card():
+    num = random.randint(0,WORDS.shape[0])
+    words = WORDS.to_dict(orient='records')
+    french_word = words[num]['French']
+    return french_word
 
 ### btns
-correct_btn = tkt.Button(window, image = CORRECT)
-wrong_btn = tkt.Button(window, image = WRONG)
+
+correct_btn = tkt.Button(window, image = CORRECT, command=next_card)
+wrong_btn = tkt.Button(window, image = WRONG, command = next_card)
+# french_wd = random_word()
+canvas.create_text(400,150, text = f'French', font=("Ariel", 40, "italic"))
+canvas.create_text(400,260, text = f'{french_word}', font=("Ariel", 60, "bold"))
 
 ### grid
 canvas.grid(row=0, column=0, columnspan=2)
 wrong_btn.grid(row= 1, column = 0)
 correct_btn.grid(row = 1, column = 1)
 
-print(WORDS)
+
 window.mainloop()
