72eb13b4187a0556d3be2bc7e1b1d1612192c6d5
jongseok <jjs0615@naver.com>
2022-03-21 07:25:53 +0900

    http requests

--- API/main.py
+++ API/main.py
@@ -2,7 +2,15 @@ import requests
 
 response = requests.get(url = "http://api.open-notify.org/iss-now.json")
 # print(response.status_code)
- 
+
+data = response.json()
+print(data)
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+iss_position = (longitude, latitude)
+print(iss_position)
+
 if response.status_code != 200:
     print("Error")
     raise Exception("Bad response from ISS API")
