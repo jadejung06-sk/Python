9e76667b5a2a921f2175de1f64330ce49168817d
jongseok <jjs0615@naver.com>
2022-03-24 22:07:35 +0900

    get question_data from API

--- API/quizzler-app-start/data.py
+++ API/quizzler-app-start/data.py
@@ -1,113 +1,125 @@
 import requests
+import html
 
-GAME_URL = "https://opentdb.com/api.php?amount=10&category=11&type=boolean"
+params = {"amount": 10, "category": 11, "type":"boolean"}
+GAME_URL = "https://opentdb.com/api.php"
 
 '''
-1. get question_data from the game_url (ing)
+1. get q uestion_data from the game_url ("https://opentdb.com/api.php?amount=10&category=11&type=boolean")
 2. make the data in the same structure
 3. run the main.py and check it right
 '''
 
+status = requests.get(url= GAME_URL, params=params).status_code
+if status:
+    # print(status)
+    question_data = requests.get(url= GAME_URL, params=params).json()['results']
+## dict_keys(['category', 'type', 'difficulty', 'question', 'correct_answer', 'incorrect_answers'])
+## need for 'question', 'correct_answer'
+    # question_list = [html.unescape(data['question'].strip()) for data in data_list]
+    # answer_list = [html.unescape(data['correct_answer'].strip()) for data in data_list]
+    # print(question_list, answer_list)
 
-question_data = [
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The HTML5 standard was published in 2014.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The first computer bug was formed by faulty wires.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Linus Torvalds created Linux and Git.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "AMD created the first consumer 64-bit processor.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "'HTML' stands for Hypertext Markup Language.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    }
-]
+
+# question_data = [
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The HTML5 standard was published in 2014.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The first computer bug was formed by faulty wires.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "Linus Torvalds created Linux and Git.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "AMD created the first consumer 64-bit processor.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "'HTML' stands for Hypertext Markup Language.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science: Computers",
+#         "type": "boolean",
+#         "difficulty": "hard",
+#         "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     }
+# ]
