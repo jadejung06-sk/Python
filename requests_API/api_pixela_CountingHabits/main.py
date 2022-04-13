48bd822963b45aaa21a64d878eb5e49c2791c98e
jongseok <jjs0615@naver.com>
2022-04-14 01:06:52 +0900

    pixela graph api

---
+++ requests_API/api_pixela_CountingHabits/main.py
@@ -0,0 +1,36 @@
+import requests
+
+## id
+#  https://pixe.la/@jadejungtest
+USERNAME = "jadejungtest"
+TOKEN = "j0a6d3e4y5y1s8pp3da4"
+GRAPH_ID = "graph123412"
+
+## endpoints
+pixela_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_value_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+## configs
+user_params = {"token": TOKEN
+, "username": USERNAME
+, "agreeTermsOfService":"yes"
+, "notMinor":"yes"}
+graph_config = {"id": GRAPH_ID,
+"name":"drinking_water_graph"
+,"unit":"liter"
+,"type":"float"
+,"color":"sora"}
+headers = {
+    "X-USER-TOKEN" : TOKEN
+}
+daily_config = {"date":"20220413",
+"quantity":"2"}
+
+## post
+# https://pixe.la/v1/users/jadejungtest/graphs/graph123412.html
+# response = requests.post(url = pixela_endpoint, json = user_params)
+graph = requests.post(url = graph_endpoint, json = graph_config, headers=headers)
+graph_point = requests.post(url=graph_value_endpoint, json = daily_config, headers=headers)
+# print(graph.text, graph_endpoint)
+print(graph_point.text)
