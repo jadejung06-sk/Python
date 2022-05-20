95e26637fbce0c6d8f73c4ccf0b645303ec35b1a
jongseok <jjs0615@naver.com>
2022-05-20 19:11:33 +0900

    logining instagram

--- se_website/se_Instagram/main.py
+++ se_website/se_Instagram/main.py
@@ -2,13 +2,14 @@ from gettext import install
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 INS_URL = "https://www.instagram.com/"
 CHROME_DRIVER_PATH = "C:\Development\chromedriver.exe"
 SIMILAR_ACCOUNT = "chefsteps"
-USERNAME = ""
-PASSWORD = ""
+USERNAME = "j-j-s07@hanmail.net"
+PASSWORD = input("Type your password:")
 
 class InstaFollower:
 
@@ -22,14 +23,26 @@ class InstaFollower:
         print("login")
         time.sleep(5)
         self.facebook_id = self.driver.find_element(By.NAME, 'email')
-        self.facebook_id.send_keys("j-j-s07@hanmail.net") # pass
+        self.facebook_id.send_keys(USERNAME) # pass
         self.facebook_pw = self.driver.find_element(By.NAME, 'pass')
-        self.facebook_pw.send_keys(input("Type your password:"))
+        self.facebook_pw.send_keys(PASSWORD)
         self.facebook_login = self.driver.find_element(By.NAME, 'login')
         self.facebook_login.click()
-        time.sleep(5)
-
+        time.sleep(10)
+        try:
+            self.alarm_btn = self.driver.find_element(By.CLASS_NAME, 'button.aOOlW.HoLwm')
+            self.alarm_btn.click()
+        except NoSuchElementException:
+            time.sleep(2)
+            print("There is no element.")
+            self.alarm_btn = self.driver.find_element(By.CLASS_NAME, 'button.aOOlW.HoLwm')
+            self.alarm_btn.click()     
+        
     def find_followers(self):
+        self.search_bar = self.driver.find_element(By.CLASS_NAME, 'input.XTCLo.d_djL.DljaH')
+        self.search_bar.send_keys(SIMILAR_ACCOUNT)
+        time.sleep(1)
+        self.search_bar.send_keys(Keys.ENTER)
         print("find")
         pass
 
