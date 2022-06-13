2ba443d5d408914ca2a23c695003bc453acf17b1
Jongseok Jung <74629459+jadejung06-sk@users.noreply.github.com>
2022-06-14 07:20:55 +0900

    Update notification_manager.py

--- requests_API/api_flight_sheety_FindGoodFlight_study/notification_manager.py
+++ requests_API/api_flight_sheety_FindGoodFlight_study/notification_manager.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.account_sid = "AC0bd2928a5eb6edb1e2252288e6ac8369"
-        self.auth_token = "d045e3b86326a31caa06310945183bde"
+        self.account_sid = ""
+        self.auth_token = ""
         # self.account_sid = os.environ['TWILIO_ACCOUNT_SID']
         # self.auth_token = os.environ['TWILIO_AUTH_TOKEN']
         self.client = Client(self.account_sid, self.auth_token)
@@ -15,8 +15,8 @@ class NotificationManager:
         self.message = self.client.messages \
                 .create(
                      body=f"Only {price:,} won to fly from Incheon-ICN to {to_city}-{to_iata}, from {from_date} to {to_date}.",
-                     from_='+13156591997',
-                     to='+821090378136'
+                     from_='+13',
+                     to='+8210'
                  )
 
 # if __name__=="__main__":
