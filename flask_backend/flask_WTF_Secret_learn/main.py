7aa039daac3fd16695c87cd0a13ca6dee28bb4a7
jongseok <jjs0615@naver.com>
2022-06-16 00:39:00 +0900

    form validator

--- flask_backend/flask_WTF_Secret_learn/main.py
+++ flask_backend/flask_WTF_Secret_learn/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField(label = 'Email')
-    password = PasswordField(label = 'Password')
+    email = StringField(label = 'Email', validators=[DataRequired(), Email(check_deliverability=True)])
+    password = PasswordField(label = 'Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit() # 
     return render_template('login.html', form = login_form)
 
 if __name__ == '__main__':
