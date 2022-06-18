9cc0b29ef1a516ee807751aac2019bc016d3aecf
jongseok <jjs0615@naver.com>
2022-06-19 00:29:58 +0900

    sqlalchemy ing

--- flask_backend/flask_SQLite_Database/main.py
+++ flask_backend/flask_SQLite_Database/main.py
@@ -12,6 +12,17 @@ app = Flask(__name__)
 # db.commit()
 
 ##### SQLAlchemy case
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////flask_backend/flask_SQLite_Database/new-books-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+class books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+book = books()
+# books = books(id=1, title='Harry Potter', author='J. K. Rowling', review=9.3 )
 
 all_books = [     
 
