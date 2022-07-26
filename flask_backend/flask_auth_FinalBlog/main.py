c1734f8e72d1c154ee70fe952de49836d9b0307f
jongseok <jjs0615@naver.com>
2022-07-26 23:18:18 +0900

    new relationships

--- flask_backend/flask_auth_FinalBlog/main.py
+++ flask_backend/flask_auth_FinalBlog/main.py
@@ -37,6 +37,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
     posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
 
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
@@ -48,6 +49,18 @@ class BlogPost(db.Model):
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
+    comments = relationship("Comment", back_populates = "parent_post")
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key = True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comment_author = relationship("User", back_populates= "comments")
+    post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
+    parent_post = relationship("BlogPost", back_populates = "comments")
+    text = db.Column(db.Text, nullable= False)   
+
+#  A string field has a limit of 255 characters, whereas a text field has a character limit of 30,000 characters.
 
 # db.drop_all()
 db.create_all()
@@ -119,10 +132,16 @@ def logout():
     return redirect(url_for('get_all_posts'))
 
 
-@app.route("/post/<int:post_id>")
+@app.route("/post/<int:post_id>", methods = ['GET', 'POST'])
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
     form = CommentForm()
+    if form.validate_on_submit():
+        new_comment = Comment(
+        text = form.commnet_text.data
+        )
+        db.session.add(new_comment)
+        db.session.commit()
     return render_template("post.html", post=requested_post, current_user=current_user, form = form)
 
 
