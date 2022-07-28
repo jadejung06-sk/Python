b72fef8f368cd04a955d7235980481984f45f415
jongseok <jjs0615@naver.com>
2022-07-29 07:23:05 +0900

    blog done

--- flask_backend/flask_auth_FinalBlog/main.py
+++ flask_backend/flask_auth_FinalBlog/main.py
@@ -2,31 +2,22 @@ from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
+from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
-from functools import wraps
 
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
 ##GRAVATAR
 gravatar = Gravatar(app,
                     size=100,
@@ -37,9 +28,15 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 ##CONFIGURE TABLES
-
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -65,9 +62,9 @@ class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    comment_author = relationship("User", back_populates= "comments")
     post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'))
     parent_post = relationship("BlogPost", back_populates = "comments")
+    comment_author = relationship("User", back_populates= "comments")
     text = db.Column(db.Text, nullable= False)   
 
 #  A string field has a limit of 255 characters, whereas a text field has a character limit of 30,000 characters.
