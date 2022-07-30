a576b1c1383e07d6cd2f45658e6901588dbc16e3
jongseok <jjs0615@naver.com>
2022-07-30 09:53:52 +0900

    Fixed untracked Files

--- flask_backend/flask_jinja_Blog2/post.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-class Post:
-    
-    def __init__(self):
-        self.blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
-        self.blog_response = requests.get(self.blog_url)
-        self.blog_data = self.blog_response.json()
--- wedding/README.md
+++
@@ -1,4 +0,0 @@
-# To prepare for a wedding.
-I wrote a wedding hall list.
-
-> commit this fom github directly.
