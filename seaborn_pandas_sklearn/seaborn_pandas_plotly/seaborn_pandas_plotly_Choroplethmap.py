52902563ed562ef60b77a49f0e3af067364e8a6e
jongseok <jjs0615@naver.com>
2022-09-05 21:48:44 +0900

    several sns plots

--- seaborn_pandas_sklearn/seaborn_pandas_plotly/seaborn_pandas_plotly_Choroplethmap.py
+++ seaborn_pandas_sklearn/seaborn_pandas_plotly/seaborn_pandas_plotly_Choroplethmap.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-s = "{'muffin' : 'lolz', 'foo' : 'kitty'}"
+# s = "{'muffin' : 'lolz', 'foo' : 'kitty'}"
 ## JsonDecodeError
 # e = json.loads(s) # json.decoder.JSONDecodeError: Expecting property name enclosed in double quotes: line 1 column 2 (char 1)
 # print(e)
@@ -10,9 +10,12 @@ s = "{'muffin' : 'lolz', 'foo' : 'kitty'}"
 # print(d)
 
 
+pd.set_option('display.max_colwidth', -1)
 
-
-# raw_json = pd.read_csv('./seaborn_pandas_sklearn/seaborn_pandas_plotly/geojson.csv')['features']
-# print(type(raw_json))
-# print(raw_json[0])
-# print(type(raw_json[0]))
+raw_json = pd.read_csv('./seaborn_pandas_sklearn/seaborn_pandas_plotly/geojson.csv')['features']
+print(type(raw_json)) # Series
+replaced_json = raw_json.apply(lambda x: x.replace("'", '\"') if "'" in x else x )
+print(type(replaced_json[0])) # str
+print(json.loads(replaced_json[107])['geometry']['coordinates']) # Correct
+# print(replaced_json[11:20].apply(lambda x : json.loads(x))) # json.decoder.JSONDecodeError: Expecting ',' delimiter: line 1 column 107 (char 106)
+# print(replaced_json[0:5])
