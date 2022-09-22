19c0286c569a7f663a2bcc5635ac5a10f52dcc99
jongseok <jjs0615@naver.com>
2022-09-23 07:17:41 +0900

    tactactoe start

---
+++ tictactoe/game.py
@@ -0,0 +1,13 @@
+ANSWER_LIST = ['O','O','O','O','O','O','O','O','O']
+QUIZ = f'''
+            |         |
+        {ANSWER_LIST[0]}   |    {ANSWER_LIST[1]}    |    {ANSWER_LIST[2]}
+    ________|_________|_________
+            |         |
+        {ANSWER_LIST[3]}   |    {ANSWER_LIST[4]}    |    {ANSWER_LIST[5]}
+    ________|_________|_________
+            |         |
+        {ANSWER_LIST[6]}   |    {ANSWER_LIST[7]}    |    {ANSWER_LIST[8]}
+            |         |
+    '''
+print(QUIZ)
