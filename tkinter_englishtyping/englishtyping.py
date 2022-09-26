cc74521d22b8938eb03b463940d49d65fa0170f0
jongseok <jjs0615@naver.com>
2022-09-26 21:54:00 +0900

    check words

---
+++ tkinter_englishtyping/englishtyping.py
@@ -0,0 +1,25 @@
+import time
+
+##### Make GUI
+
+##### Time
+start_tm = time.time()
+##### Count words
+test_string = "student teacher dictionary instead type fast average ten words per minute adults should aim once you know speed test check time practice track progress explicitly require certain shorter distances across pointer fingers cozy food activities mental health strategies spend quality time  newfound commitment action plan helps success classrooms stands under leadership decisions graduate automated rostering transform secure metrics administrative permissions internet connection evidence technology latest keyboarding pathway essential documentation support detailed education tests certificates growth robust unified approach correct"
+input_string = input('Testing : ')
+
+test_list = test_string.split(' ')
+input_list = input_string.split(' ')
+print(f'test_list : {test_list}')
+print(f'input_list : {input_list}' )
+
+# print(test_list == input_list)
+cnt_word = len(test_list)
+##### Check the last word
+
+
+
+end_tm =  time.time() - start_tm 
+print(f'Time : {end_tm:.2f} s')
+##### Calculate words per minute
+print(f'WPM : { round(cnt_word/end_tm*60, 0)}')
