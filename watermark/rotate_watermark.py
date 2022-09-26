ea35633a0f355e5d5906f170de74df0abc73114a
jongseok <jjs0615@naver.com>
2022-09-26 20:53:22 +0900

    modified watermark tkinter

--- watermark/rotate_watermark.py
+++ watermark/rotate_watermark.py
@@ -1,4 +1,3 @@
-
 from PIL import ImageFont, ImageDraw, ImageOps, Image
 import os
 
@@ -11,7 +10,7 @@ for jpgpath in jpgpath_list:
         # img=Image.open("D:/Python/watermark/2021-10-25-15-35-48-464.jpg")
         width, height = img.size
 
-        text = "종석 ♥ 영선"
+        text = "♥"
         font = ImageFont.truetype('C:/Users/jjs06/AppData/Local/Microsoft/Windows/Fonts/NanumGothicCoding.ttf', 100)
         txt=Image.new('L', (width,height))
         print(txt)
@@ -25,9 +24,6 @@ for jpgpath in jpgpath_list:
             d.text( (x-1000, y), text,  font=font, fill=255)
             d.text( (x-2000, y), text,  font=font, fill=255)
             d.text( (x-3000, y), text,  font=font, fill=255)
-
-            # d.text( (x, y), text,  font=font, fill=255)
-            # d.text( (x, y), text,  font=font, fill=255)
             w=txt.rotate(45,  expand=1)
             img.paste( ImageOps.colorize(w, (0,0,0), (000, 255, 255)), (242,60),  w)
             img.show()
