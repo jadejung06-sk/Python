1dbc8da31ba2d8a4b47614a024e066dc580a65f4
jongseok <jjs0615@naver.com>
2023-04-13 18:33:43 +0900

    click and send_keys

--- WebCrawling/WebDriver_2304/Gettingstarted.py
+++ WebCrawling/WebDriver_2304/Gettingstarted.py
@@ -4,4 +4,22 @@
 # 111.0.5563.147 (64 bit) - Windows
 ## 2. Down load Selenium with Python - Chrome Driver
 # chromedriver.exe
-# pip install selenium
+'''
+pip install selenium
+'''
+## 3.1. webdriver.Chrome() Error
+# read device interface GUIDs: 지정된 파일을 찾을 수 없습니다. (0x2)    
+# DeprecationWarning: executable_path has been deprecated, please pass in a Service object
+# >>> https://scribblinganything.tistory.com/463
+'''
+options = wd.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = wd.Chrome(options=options, executable_path=PATH)
+'''
+## 3.2. driver.find_elements_by_id()
+# DeprecationWarning: find_element_by_* commands are deprecated. Please use find_element() instead
+# By.ID
+# >>> https://dejavuqa.tistory.com/109
+'''
+from selenium.webdriver.common.by import By
+'''
--- WebCrawling/WebDriver_2304/run.py
+++ WebCrawling/WebDriver_2304/run.py
@@ -3,22 +3,28 @@
 # PC web Site or Mobile
 ## 2. pip install selenium
 from selenium import webdriver as wd
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 ## 3. Load driver
 # Option : no images, control agent, proxy
 # Check temporary files of driver capacity
 PATH = './WebCrawling/WebDriver_2304/chromedriver.exe'
 MAIN_URL = 'http://tour.interpark.com/'  # + slash
 keywords = ['로마']
-
-driver = wd.Chrome(executable_path=PATH)
+options = wd.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = wd.Chrome(options=options, executable_path=PATH)
+# driver = wd.Chrome(PATH)
 ## 4. Connect on Site
 driver.get(MAIN_URL)
 ## 5. Point Searching Button and Typing some words
 # ID > Class > Paraent > Sibbling
 # ID divHeaderSearch
 # element or elements
-driver.find_element_by_id(id_ = "divHeaderSearch")#.send_keys(keywords[0])
-
+driver.find_element(by = By.ID, value = "divHeaderSearch").click()
+time.sleep(10)
+driver.find_element(by = By.ID, value = "txtHeaderInput").send_keys(keywords[0])
 
 ## 6. Click on searching button
 
