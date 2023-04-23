56ca810a58264f0e5cd4977b31e96da44131fb85
jongseok <jjs0615@naver.com>
2023-04-23 22:26:37 +0900

    8 times using threads with mutex

--- multithreading/letter_counter.py
+++ multithreading/letter_counter.py
@@ -1,31 +1,42 @@
 import json
 import urllib.request
 import time
-from threading import Thread
+from threading import Thread, Lock
 
 finished_counter = 0
 
 
-def counter_letter(url, frequency):
+def counter_letter(url, frequency, mutex):
     response = urllib.request.urlopen(url)
     txt = str(response.read())
+    mutex.acquire()
     for l in txt:
         letter = l.lower()
         if letter in frequency:
             frequency[letter] += 1
     global finished_counter
     finished_counter += 1
+    mutex.release()
             
 def main():
     frequency = {}
+    mutex = Lock()
     for c in "abcdefghijklmnopqrstuvwxyz":
         frequency[c] = 0
     start = time.time()
     for i in range(1000, 1020):
-        Thread(target = counter_letter, args= (f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)).start()
+        Thread(target = counter_letter, args= (f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency, mutex)).start()
         # counter_letter(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)
     # print(finished_counter)
-    while finished_counter < 20:
+    ##### w/o mutex
+    # while finished_counter < 20:
+        # time.sleep(0.5)
+    ##### with mutex
+    while True:
+        mutex.acquire()
+        if finished_counter == 20:
+            break
+        mutex.release()
         time.sleep(0.5)
     end = time.time()
     print(json.dumps(frequency, indent = 4))
@@ -35,9 +46,12 @@ main()
 
 
 '''
+### 1
     "d": 40501, 
     "e": 140093,
 Not exact but quicker (Due to Race condition, quite rare)
-
 Need syncronization 
+### 2
+using mutex
+Done, time taken 1.79 s vs. 16.1 s (single thread)
 '''
