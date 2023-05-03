9bf0e65b29565827327739f063c5768f0f31c71b
jongseok <jjs0615@naver.com>
2023-05-03 21:13:36 +0900

    simple pipe

--- parallel_compute/multiprocess/understand_ping_pong_pipe.py
+++ parallel_compute/multiprocess/understand_ping_pong_pipe.py
@@ -20,5 +20,5 @@ def pong(pipe_conn):
         
 if __name__ == '__main__':
     pipe_end_a, pipe_end_b = Pipe()
-    Process(target = ping, args = (pipe_end_a, ))
-    Process(target = pong, args = (pipe_end_b, ))
+    Process(target = ping, args = (pipe_end_a, )).start()
+    Process(target = pong, args = (pipe_end_b, )).start()
