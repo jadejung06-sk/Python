c2e4dc430a3689f22c9c22f7fbfc2648a4bda0ee
jongseok <jjs0615@naver.com>
2023-05-05 19:06:39 +0900

    wind_direc single process

--- parallel_compute/multiprocess/wind_direction_single_process.py
+++ parallel_compute/multiprocess/wind_direction_single_process.py
@@ -1,11 +1,12 @@
 import re
 import time
+import os
 
 WIND_REGEX = "\d* METAR.*EGLL \d*Z [A-Z ]*(\d{5}KT|VRB\d{2}KT).*="
 WIND_EX_REGEX = "(\d{5}KT|VRB\d{2}KT)"
 VARIABLE_WIND_REGEX = ".*VRB\d{2}KT"
 VALID_WIND_REGEX = "\d{5}KT"
-WIND_DIR_ONLY_REGEX = "(\d{3}\d{2}KT)"
+WIND_DIR_ONLY_REGEX = "(\d{3})\d{2}KT"
 TAF_REGEX = ".*TAF.*"
 COMMENT_REGEX = "\w*#.*"
 METAR_CLOSE_REGEX = ".*="
@@ -28,9 +29,9 @@ def parse_to_array(text):
 def extract_wind_direction(metars):
     winds = []
     for metar in metars:
-        if re.search(WIND_REGEX, metar):
+        if re.search(WIND_REGEX, metar): 
             for token in metar.split():
-                if re.match(WIND_EX_REGEX, token): winds.append(re.match(WIND_EX_REGEX, token).group(1))
+                if re.match(WIND_EX_REGEX, token): winds.append(re.match(WIND_EX_REGEX, token).group(1)) # '36007KT'
     return winds
 
 def mine_wind_distribution(winds, wind_dist):
@@ -43,3 +44,26 @@ def mine_wind_distribution(winds, wind_dist):
             dir_index = round(d / 45.0) % 8
             wind_dist[dir_index] += 1
     return wind_dist
+
+if __name__ == '__main__':
+    path_with_files = './parallel_compute/multiprocess/metarfiles'
+    # print(os.getcwd()) # D:\2022\Python
+    # print(os.listdir(path_with_files)) # 
+    wind_dist = [0] * 8 
+    start = time.time()
+    for file in os.listdir(path_with_files):
+        f = open(os.path.join(path_with_files, file), "r")
+        text = f.read()
+        metars = parse_to_array(text)
+        winds = extract_wind_direction(metars)
+        wind_dist = mine_wind_distribution(winds, wind_dist)
+    end = time.time()
+    # print(winds)
+    print(wind_dist)
+    print("Time taken", end - start)
+    
+    '''
+    [32011, 34088, 32962, 23342, 38025, 54267, 57645, 28746]
+    Time taken 4.104559421539307
+    '''
+    
