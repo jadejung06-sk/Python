55be7d68322cc120e82af54fffcc2a39263462f8
jongseok <jjs0615@naver.com>
2023-05-12 06:16:58 +0900

    time

--- WebCrawling/apple_website/time.py
+++ WebCrawling/apple_website/time.py
@@ -1,6 +1,12 @@
 import time
 
-print(time.time()) # Epoch Time
+print(time.time()) # Epoch/Unix Time
 print(time.ctime(time.time()))
 
+current_time = time.localtime()
+current_time = time.strftime('%Y year %m month %d day', current_time)
+print(current_time)
 
+import datetime
+simple_date = datetime.datetime(2023, 5, 12, 6, 15, 30)
+print(simple_date)
