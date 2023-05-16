2acea43500227d7a6078abd27e9a4ad24349bbe6
jongseok <jjs0615@naver.com>
2023-05-16 13:59:13 +0900

    ing searching imgs

--- WebCrawling/apple_website/se_instagram/se_app.py
+++ WebCrawling/apple_website/se_instagram/se_app.py
@@ -10,6 +10,7 @@
  'C:\\Users\\jjs06\\AppData\\Local\\Programs\\Python\\Python310\\lib\\site-packages\\win32\\lib',
  'C:\\Users\\jjs06\\AppData\\Local\\Programs\\Python\\Python310\\lib\\site-packages\\Pythonwin']
 # sys.path.append('D:\\2022\\Python\\WebCrawling\\apple_website\\se_instagram')
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import sys
@@ -23,24 +24,25 @@ from gitignore.personal import Personal
 personal = Personal()
 # ID, PW = personal._get_info('instagram')
 ID, PW = personal._get_info('facebook')
-
 driver = webdriver.Chrome('./WebCrawling/apple_website/se_instagram/chromedriver.exe')
 driver.get('https://instagram.com')
-time.sleep(3)
 
+time.sleep(3)
 facebook_click = driver.find_element_by_css_selector('._ab37').click()
 time.sleep(5)
-
 id_input = driver.find_element_by_class_name('clearfix #email')
 id_input.send_keys(ID)
 time.sleep(2)
 pw_input = driver.find_element_by_class_name('clearfix #pass')
 pw_input.send_keys(PW)
 time.sleep(2)
-pw_input.send_keys(Keys.Enter)
-
-
+pw_input.send_keys(Keys.ENTER)
 # e = driver.find_element_by_css_selector('div ._aa4u').text
 # print(e)
-
-##### Instgram
+#### Instgram
+# Click the first image
+# driver.implicitly_wait(10)
+## Move on a page
+driver.get('https://www.instagram.com/explore/tags/%EC%82%AC%EA%B3%BC/')
+search_apple = driver.find_element_by_css_selector('._aagw')
+## Next page
