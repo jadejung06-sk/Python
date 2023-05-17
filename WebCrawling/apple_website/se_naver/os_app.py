ba89e655fde0147b41128102bf2cf7a7c5d31977
jongseok <jjs0615@naver.com>
2023-05-17 20:00:52 +0900

    os and shutil

--- WebCrawling/apple_website/se_naver/os_app.py
+++ WebCrawling/apple_website/se_naver/os_app.py
@@ -1,1 +1,14 @@
 import os
+import shutil
+
+files = os.listdir(r'D:\2022\Python\WebCrawling\apple_website\se_naver\file_test')
+
+##### rename and copy
+# for file in files:
+    # os.rename(f'D:/2022/Python/WebCrawling/apple_website/se_naver/file_test/{file}', f'D:/2022/Python/WebCrawling/apple_website/se_naver/file_test/a{file}')
+    
+# for file in files:
+    # shutil.copy(f'D:/2022/Python/WebCrawling/apple_website/se_naver/file_test/{file}', f'D:/2022/Python/WebCrawling/apple_website/se_naver/file_test2/a{file}')
+    
+##### os.path.join
+print(os.path.join(os.getcwd(), 'test')) # D:\2022\Python\test
