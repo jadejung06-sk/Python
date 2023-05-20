192935bf52d458ba598ae5c6a638165d8c60414a
jongseok <jjs0615@naver.com>
2023-05-20 15:53:09 +0900

    regex done

--- WebCrawling/apple_website/re_learning/re_app.py
+++ WebCrawling/apple_website/re_learning/re_app.py
@@ -1,7 +1,7 @@
 import re
 
 ##### simple
-target = '5abcde$3567^g123'
+target = '5AAaaabcde$3567^g123'
 a_obj = re.search('abc', 'abcdefg') # object
 a_lst = re.findall('abc', 'abcdefg') # list
 print(a_obj)
@@ -30,8 +30,39 @@ print('or: ', se, alphabet_all, alphabet_simple, korean)
 not_numbers = re.findall('[^0-9]', target)
 print('not []: ', not_numbers)
 
-##### sepcial
+##### sepcial numbers
 num1 = re.findall('\d', target) # 5abcde$3567^g123 ['5', '3', '5', '6', '7', '1', '2', '3'] 
 num2 = re.findall('\d\d', target) # 5abcde$3567^g123 ['35', '67', '12'] 
 num3 = re.findall('\d\d\d', target) # 5abcde$3567^g123 ['356', '123']
-print(f'special \d: {target} vs. ', num1, num2, num3)
+num3_multi = re.findall('\d{3}', target) # 5abcde$3567^g123 ['356', '123']
+not_num1 = re.findall('\D', target) # 5abcde$3567^g123 ['35', '67', '12']
+print(f'special \d: {target} vs. ', num1, num2, num3, num3_multi, not_num1)
+
+##### special spaces
+space1 = re.findall('\s', target)
+not_space = re.findall('\S', target)
+print(f'special \s: {target} vs. ', space1, not_space)
+
+##### + * greedy
+greedy_plus = re.findall('a+', target)
+## capital
+capital_words = re.findall('ABC', target, re.IGNORECASE)
+print(f'greedy_plus: {target} vs. ', greedy_plus) # ['aaa']
+print(f'capital: {target} vs. ', capital_words) # ['abc']
+#########################
+
+##### substi.
+org_time = '2023-01-01'
+chg_time = re.sub('-', '.', org_time)
+print(org_time, 'vs.', chg_time)
+
+new_target = re.sub('\d', '', target)
+print(target, 'vs.', new_target)
+
+##### test
+test1 = re.findall('a', target)
+test2 =re.findall('^a', target)
+test3 = re.findall('[\d\W]', target)
+print(test1, test2, test3)
+
+
