413cca496bcb373b9d12a2b644546bb231ba15ad
jongseok <jjs0615@naver.com>
2023-05-20 10:54:40 +0900

    learning regex

--- WebCrawling/apple_website/re_learning/re_app.py
+++ WebCrawling/apple_website/re_learning/re_app.py
@@ -1,7 +1,7 @@
 import re
 
 ##### simple
-target = 'abcde$f^g'
+target = '5abcde$3567^g123'
 a_obj = re.search('abc', 'abcdefg') # object
 a_lst = re.findall('abc', 'abcdefg') # list
 print(a_obj)
@@ -16,8 +16,22 @@ print(g_last)
 ##### special symbols
 dol = re.findall('\$', target)
 cur = re.findall('\^', target)
-print(dol, cur)
+print('symbols: ', dol, cur)
 
 ##### or
 se = re.findall('[abc]', target) # each words
-print(se)
+alphabet_all = re.findall('[abcdefghijklmnopqrstuvwxyzABCDEFG]', target) # each words
+alphabet_simple = re.findall('[a-zA-Z]', target) # each words
+numbers = re.findall('[0-9]', target)
+korean = re.findall('[가-힣ㄱ-ㅎ]', target)
+print('or: ', se, alphabet_all, alphabet_simple, korean)
+
+##### not []
+not_numbers = re.findall('[^0-9]', target)
+print('not []: ', not_numbers)
+
+##### sepcial
+num1 = re.findall('\d', target) # 5abcde$3567^g123 ['5', '3', '5', '6', '7', '1', '2', '3'] 
+num2 = re.findall('\d\d', target) # 5abcde$3567^g123 ['35', '67', '12'] 
+num3 = re.findall('\d\d\d', target) # 5abcde$3567^g123 ['356', '123']
+print(f'special \d: {target} vs. ', num1, num2, num3)
