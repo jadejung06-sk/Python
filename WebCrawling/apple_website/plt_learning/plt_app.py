62b90ffc1ff6692424a9ca6aa6ad18414994ef93
jongseok <jjs0615@naver.com>
2023-05-21 12:16:57 +0900

    plot and bar

--- WebCrawling/apple_website/plt_learning/plt_app.py
+++ WebCrawling/apple_website/plt_learning/plt_app.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 
 
+##### multiple line plots in a graph
+plt.figure(figsize = (20, 10)) # inch
+plt.plot([1,2,3], [10,20,30])
+plt.plot([1,2,3], [20,40,60])
+plt.plot([1,2,3], [30,60,90])
+plt.show()
+
 ##### line plot : time series
 plt.plot([1,2,3], [4,5,6]) # x y list
 # plt.plot(df.index, df['Close'], color = 'crimson") # x y Series object
@@ -28,3 +35,22 @@ math = [5,8, 23, 5, 67, 34, 34, 23]
 eng = [23,6, 3, 1, 5, 45, 54, 34]
 plt.scatter(math, eng)
 plt.show()
+
+##### stack plot : accumulation of data
+plt.stackplot([1,2,3], [10, 20, 30], [30,20,50]) # x y1 y2 y3 ...
+plt.show()
+
+
+##### test : label, legend, multiple line plots
+# plt.plot(df1.index, df1['rolling5'])
+# plt.plot(df1.index, df1['rolling20'])
+# plt.plot(df1.index, df1['rolling60'])
+# plt.xlabel('Time')
+# plt.ylabel('Price')
+# plt.legend(['rolling5', 'rolling20', 'rolling60'])
+# plt.show()
+
+##### test : bar chart using filtered data
+# plt.figure(figsize = (20, 10))
+# plt.bar(df1[df1['Volume'] > df1['Volume'].mean()]['Volume'].index, df1[df1['Volume'] > df1['Volume'].mean()]['Close'])
+# plt.show()
