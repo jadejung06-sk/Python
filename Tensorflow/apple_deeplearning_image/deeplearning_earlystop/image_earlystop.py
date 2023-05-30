6983d2bd68c1f6853e25c64ee5bc7da9eee00760
jongseok <jjs0615@naver.com>
2023-05-30 21:23:58 +0900

    tensorboard

---
+++ Tensorflow/apple_deeplearning_image/deeplearning_earlystop/image_earlystop.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import time
+
+
+
+##### Data
+(trainX, trainY), (testX, testY) = tf.keras.datasets.fashion_mnist.load_data()
+
+trainX = trainX / 255.0
+testX = testX / 255.0
+
+trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+testX = testX.reshape((testX.shape[0], 28, 28, 1))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), padding = "same", activation = 'relu', input_shape = (28, 28, 1)),
+    tf.keras.layers.MaxPooling2D( (2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation= "relu"),
+    tf.keras.layers.Dense(10, activation= 'softmax') # no need to activation, num of categories 10
+])
+model.summary()
+##### Tensorboard
+# tensorboard = tf.keras.callbacks.TensorBoard(log_dir='logs/{}'.format('~~' + str(int(time.time()))) )
+# model.fit(어쩌구, callbacks=[tensorboard]) 
+
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics= ['accuracy'] )
+model.fit(trainX, trainY, validation_data = (testX, testY), epochs = 3)
+
+
+
