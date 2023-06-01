e64649279722c05485160e845dd56b82133d7d1c
jongseok <jjs0615@naver.com>
2023-06-01 21:21:40 +0900

    pretrained

--- Tensorflow/apple_deeplearning_image/kaggle_image_dogscats/deep_image_kaggle_dogcat.py
+++ Tensorflow/apple_deeplearning_image/kaggle_image_dogscats/deep_image_kaggle_dogcat.py
@@ -18,7 +18,7 @@ import shutil
 #         shutil.copyfile("D:/2022/Python/Tensorflow/apple_deeplearning/kaggle_image_dogscats/train/" + i , "D:/2022/Python/Tensorflow/apple_deeplearning/kaggle_image_dogscats/dataset/dog/" + i)
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     "D:/2022/Python/Tensorflow/apple_deeplearning_image/kaggle_image_dogscats/dataset/",
-    image_size = (64, 64),
+    image_size = (150, 150),
     batch_size= 64,
     subset = 'training',
     validation_split = 0.2,
@@ -27,7 +27,7 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     "D:/2022/Python/Tensorflow/apple_deeplearning_image/kaggle_image_dogscats/dataset/",
-    image_size = (64, 64),
+    image_size = (150, 150),
     batch_size= 64,
     subset = 'validation',
     validation_split = 0.2,
@@ -103,10 +103,36 @@ val_ds = val_ds.map(standarization)
 from keras.applications.inception_v3 import InceptionV3
 #inception_model = InceptionV3(input_shape= (299, 299, 3)) # origin
 ### Image Analysis
-inception_model = InceptionV3(input_shape=(150,150,3), include_top= False, weights = None) # top = output layer
+inception_model = InceptionV3(input_shape=(150,150,3), include_top= False, weights = None) # top = output layer, weights w/ file
 inception_model.load_weights('D:/2022/Python/Tensorflow/apple_deeplearning_image/kaggle_image_dogscats/inception_v3.h5')
 inception_model.summary()
-### no training
+### method 1-1 : all layers, no training == w/ file 
 for i in inception_model.layers:
-    i.trainable = False
+    i.trainable = False # i == layers
+### method 1-2 : all layers, some of training == w/ file (fine tunning) 
+unfreeze = False
+for i in inception_model.layers:
+    if i.name == 'mixed6':
+        unfreeze = True
+    if unfreeze == True:
+        i.trainable = True
+    
+### a single layer
+lastlayer = inception_model.get_layer('mixed7')
+print(lastlayer)
+print(lastlayer.output)
+print(lastlayer.output_shape)
+### my layers (all x possible)
+layer1 = tf.keras.layers.Flatten()(lastlayer.output) #
+layer2 = tf.keras.layers.Dense(1024, activation='relu')(layer1)
+drop1 = tf.keras.layers.Dropout(0.2)(layer2)
+layer3 = tf.keras.layers.Dense(1, activation='sigmoid')(drop1) # binary
+
+model = tf.keras.Model(inception_model.input, layer3)
+
+### learning rate (fine tunning) 
+# model.compile(loss= 'binary_crossentropy', optimizer = 'adam', metrics = ['acc']) - loss: 0.0427 - acc: 0.9837 - val_loss: 0.0936 - val_acc: 0.9716
+model.compile(loss= 'binary_crossentropy', optimizer = tf.keras.optimizers.Adam(lr=0.00001), metrics = ['acc'])
+model.fit(train_ds, validation_data = val_ds, epochs = 2)
+
 ### Image Categorization
