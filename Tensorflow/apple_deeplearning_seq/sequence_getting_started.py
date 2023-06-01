924286a8c69711742a17ac96525a0d00fa068e27
jongseok <jjs0615@naver.com>
2023-06-01 21:26:57 +0900

    RNN

---
+++ Tensorflow/apple_deeplearning_seq/sequence_getting_started.py
@@ -0,0 +1,11 @@
+# 1. vector to sequence (이미지 자동 캡션)
+# 2. sequence to vector (글의 감정분석하기, 악플검사하기)
+# 3. sequence to sequence (번역)
+
+
+# Simple Recurrent Neural Network (재발하는)
+# : Input – activation – output
+# : low accuracy due to diminishing gradient (첫 글자의 중요도가 희석)
+
+# Long Short Term Memory
+# H1 & CELL STATE
