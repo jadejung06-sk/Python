a103e2f7286eab3dcf676e08a9740d4904b43da8
jongseok <jjs0615@naver.com>
2023-07-09 16:04:59 +0900

    venv

---
+++ inflearn/venv/venv.py
@@ -0,0 +1,24 @@
+##### make virtual Environments
+## activate
+## deactivate
+'''
+python -m venv p_study
+cd p_study
+cd Scripts
+activate
+code  /  jupyter notebook
+# (p_study)
+# later delete p_study folder
+deactivte.bat
+'''
+
+##### package
+## pip list
+## pip install
+## path : p_study/Lib/site-packages/
+'''
+pip list
+pip install --upgrade pip
+pip install pendulum # python timestamp
+pip install pytest
+'''
