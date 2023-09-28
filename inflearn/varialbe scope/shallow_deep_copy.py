e2fe261a35e67bab9b2e98bfe47e6e5fabc09a88
jongseok <jjs0615@naver.com>
2023-09-28 20:17:20 +0900

    shallow deep copy

--- inflearn/varialbe scope/shallow_deep_copy.py
+++ inflearn/varialbe scope/shallow_deep_copy.py
@@ -4,9 +4,10 @@
 가변 : list, set, dict
 불변 : tuple, int, str, float, bool, unicode
 """
+### call by value, call by reference, call by share
 
 ##### Ex 1 - copy (중요)
-### call by value, call by reference, call by share
+## call by reference
 a_list = [1, 2, 3, [4, 5, 6], [7, 8, 9]]
 b_list = a_list
 print('Ex 1 > ', id(a_list), id(b_list))
@@ -17,9 +18,26 @@ print('Ex 1 > ', id(a_list), id(b_list), a_list, b_list)
 b_list[3][2] = 100
 print('Ex 1 > ', id(a_list), id(b_list), a_list, b_list)
 
-##### Ex 2 - shallow copy
+##### Ex 2 - shallow copy (중요)
+## call by value
 import copy
+c_list = [1, 2, 3, [4, 5, 6], [7, 8, 9]]
+d_list = copy.copy(c_list)
+print('Ex 2 > ', id(c_list), id(d_list)) # 1503908096256 1503905131712
 
+d_list[1] = 100 ## different reference
+print('Ex 2 > ', id(c_list), id(d_list), c_list, d_list) 
 
+d_list[3].append(1000) ## inner same reference
+d_list[4][1] = 10000
+print('Ex 2 > ', id(c_list), id(d_list), c_list, d_list) ## [1, 2, 3, [4, 5, 6, 1000], [7, 10000, 9]] [1, 100, 3, [4, 5, 6, 1000], [7, 10000, 9]]  
 
 ##### Ex 3 - deep copy
+e_list = [1, 2, 3, [4, 5, 6], [7, 8, 9]]
+f_list = copy.deepcopy(e_list) ## inner call by reference
+
+print('Ex 3 > ', id(e_list), id(f_list))
+
+f_list[3].append(1000)
+f_list[4][1] = 10000
+print('Ex 3 > ', id(e_list), id(f_list), e_list, f_list)
