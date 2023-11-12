dba4d16674d49a2863a4661783b8f0714bdc972f
jongseok <jjs0615@naver.com>
2023-11-12 22:04:16 +0900

    multiprocessing Pool

--- inflearn/concurrency/iobound_multiprocessing.py
+++ inflearn/concurrency/iobound_multiprocessing.py
@@ -22,38 +22,40 @@ Async IO : Single process, single thread, cooperative multitasking, tasks cooper
 
 I/O Bound, requests 
 '''
-##### I/O Bound GIL
-import concurrent.futures
-import threading
+##### I/O Bound Multiprocessing Pool 예제
+## 실행 순서를 보장 받을 수 없음
+import multiprocessing
 import requests
 import time
 
-## 각 스레드에 생성되는 객체(독립된 네임스페이스)
-thread_local = threading.local()
+## 각 프로세스 메모리 영역에 생성되는 객체(독립적)
+## 함수 실행할 때 마다 객체 생성이 좋지 않기에 각 프로세스를 최초로 할당할 때 객체를 생성하는 것이 유리
 
-def get_session():
-    if not hasattr(thread_local, "session"): ## attribute가 존재하는지 묻는 함수, dict으로 추가
-        thread_local.session = requests.Session()
-    return thread_local.session
+session = None
+
+def set_global_session():
+    global session ## 있으면 사용
+    if not session: ## 없으면 새로 활용
+        session = requests.Session()
         
 ## 실행함수1 (다운로드)
 def request_site(url): ##
     
-    session = get_session() 
     ## 세션 확인
     print(session)
     # print(session.headers)
     with session.get(url) as response:
-        print(f'[Read Contents : {len(response.content)}, status Code : {response.status_code}] from {url}')
+        name = multiprocessing.current_process().name
+        print(f'[{name} -> Read Contents : {len(response.content)}, status Code : {response.status_code}] from {url}')
 
 ## 실행함수2 (요청)
 def request_all_sites(urls):
-    ## 멀티스레드 실행
-    ## 반드시 max_worker 개수 조절 후 session 객체 확인
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        executor.map(request_site, urls)
+    ## 멀티프로세싱 실행
+    ## 반드시 processes 개수 조절 후 session 객체 및 실행 시간 확인
+    with multiprocessing.Pool(initializer=set_global_session, processes=4) as pool:
+        pool.map(request_site, urls)
         '''
-        requst_site(url, session)
+        request_site(url, session)
         Downloaded 9 sites in 0.006001472473144531 seconds
         '''
 
