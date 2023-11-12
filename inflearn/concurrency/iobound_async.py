540802f0fb93d4c48fcd5e35f22a6f721bad59b6
jongseok <jjs0615@naver.com>
2023-11-13 06:31:29 +0900

    aysnc aiohttp ayncio

--- inflearn/concurrency/iobound_async.py
+++ inflearn/concurrency/iobound_async.py
@@ -24,41 +24,51 @@ I/O Bound, requests
 '''
 ##### I/O Bound Async 예제
 ## threading 보다 코드 복잡도가 높음 -> Async, Await 적절하게 작성 필요
+'''pip install aiohttp'''
 import asyncio
 import multiprocessing
-import requests
+import aiohttp ## sync
 import time
 
 ## 각 프로세스 메모리 영역에 생성되는 객체(독립적)
 ## 함수 실행할 때 마다 객체 생성이 좋지 않기에 각 프로세스를 최초로 할당할 때 객체를 생성하는 것이 유리
 
-session = None
-
-def set_global_session():
-    global session ## 있으면 사용
-    if not session: ## 없으면 새로 활용
-        session = requests.Session()
-        
+       
 ## 실행함수1 (다운로드)
-def request_site(url): ##
+async def request_site(session, url): ##
     
     ## 세션 확인
     print(session)
     # print(session.headers)
-    with session.get(url) as response:
-        name = multiprocessing.current_process().name
-        print(f'[{name} -> Read Contents : {len(response.content)}, status Code : {response.status_code}] from {url}')
+    # with session.get(url) as response:
+    #     name = multiprocessing.current_process().name
+    #     print(f'[{name} -> Read Contents : {len(response.content)}, status Code : {response.status_code}] from {url}')
+
+    async with session.get(url) as response:
+        print('Read Contents {0}, from {1}'.format(response, url))
 
 ## 실행함수2 (요청)
-def request_all_sites(urls):
+async def request_all_sites(urls):
     ## 멀티프로세싱 실행
     ## 반드시 processes 개수 조절 후 session 객체 및 실행 시간 확인
-    with multiprocessing.Pool(initializer=set_global_session, processes=4) as pool:
-        pool.map(request_site, urls)
-        '''
-        request_site(url, session)
-        Downloaded 9 sites in 0.006001472473144531 seconds
-        '''
+    async with aiohttp.ClientSession() as session:
+        ## 작업 목록
+        tasks = []
+        for url in urls:
+            task = asyncio.ensure_future(request_site(session, url))
+            tasks.append(task)
+        ## 테스크 확인
+        # print(*tasks)
+        # print(tasks)
+        
+        await asyncio.gather(*tasks, return_exceptions=True)
+    
+    # with multiprocessing.Pool(initializer=set_global_session, processes=4) as pool:
+        # pool.map(request_site, urls)
+        # '''
+        # request_site(url, session)
+        # Downloaded 9 sites in 0.006001472473144531 seconds
+        # '''
 
 
 def main():
@@ -69,7 +79,7 @@ def main():
     start_time = time.time()
 
     ## 실행
-    request_all_sites(urls)   
+    asyncio.get_event_loop().run_until_complete(request_all_sites(urls))
     
     ## 실행 시간 종료
     duration = time.time() - start_time
@@ -79,5 +89,5 @@ def main():
     print(f'Downloaded {len(urls)} sites in {duration} seconds')
     
 if __name__ == "__main__":
-    # main()
-    asyncio.run(main())
+    main()
+    # asyncio.run(main())
