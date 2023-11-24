7588a902f058fb02da5f0c4c9b5d5a89af59f30c
jongseok <jjs0615@naver.com>
2023-11-25 06:53:06 +0900

    isdecimal

--- inflearn/algorithm/extract_int.py
+++ inflearn/algorithm/extract_int.py
@@ -3,8 +3,9 @@
 출력한 숫자의 약수 개수를 다음주에 출력
 120
 '''
+##### My solution
 import sys
-# sys.stdin = open("D:/2022/Python/inflearn/algorithm/grade/input.txt", "rt")
+sys.stdin = open("D:/2022/Python/inflearn/algorithm/grade/input.txt", "rt")
 n = input()
 digits = ''
 for i in n:
@@ -18,4 +19,19 @@ for val in range(1, digits//2+1):
     if digits % val == 0:
        cnt += 1
     #    print(digits, val, digits % val, cnt)
-print(cnt) 
+print(cnt)
+
+##### Solution
+import sys
+sys.stdin = open("D:/2022/Python/inflearn/algorithm/grade/input.txt", "rt")
+n = input()
+res = 0
+for x in n:
+    if x.isdecimal():
+        res = res*10 + int(x)
+print(res)
+cnt = 0
+for val in range(1, res+1):
+    if res % val == 0:
+       cnt += 1
+print(cnt)
