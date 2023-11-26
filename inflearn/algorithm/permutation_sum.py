19fb544fc93b1898a464d0570436f15b903563a6
jongseok <jjs0615@naver.com>
2023-11-26 15:15:57 +0900

    permutation sum

--- inflearn/algorithm/permutation_sum.py
+++ inflearn/algorithm/permutation_sum.py
@@ -1,6 +1,7 @@
 '''
 n개의 순열이 존재하고, n을 앞에서 부터 더해가면서, m이 되는 경우의 수를 구하라.
 '''
+##### My solution
 import sys
 sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
 n, m = map(int, input().split())
@@ -18,3 +19,29 @@ while n >= end_idx:
     else:
         end_idx += 1
 print(cnt)
+
+
+##### Solution
+# import sys
+# sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
+# n, m = map(int, input().split())
+# a = list(map(int, input().split()))
+# lt = 0
+# rt = 1
+# tot = a[0]
+# cnt = 0
+# while True:
+#     if tot < m:
+#         if rt < n :
+#             tot += a [rt]
+#             rt += 1
+#         else:
+#             break
+#     elif tot == m:
+#         cnt += 1
+#         tot -= a[lt]
+#         lt += 1
+#     else:
+#         tot -= a[lt]
+#         lt += 1
+# print(cnt)
