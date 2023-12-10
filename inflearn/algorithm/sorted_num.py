8fbbd474540fdb4ccad2cb1f8b4446782937d050
jongseok <jjs0615@naver.com>
2023-12-10 22:12:03 +0900

    two division

---
+++ inflearn/algorithm/sorted_num.py
@@ -0,0 +1,41 @@
+'''
+몇 개의 숫자와 특정 숫자를 알려준다.
+그 숫자가 오름차순 정렬에서 몇 번째인지 구하라.
+'''
+import sys
+sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+lt = 0
+rt = len(a)
+mid = (rt + lt) // 2
+while a[mid] != m:
+    mid = (rt + lt) // 2
+    # print(mid)
+    if a[mid] < m:
+        lt = mid + 1
+    else:
+        rt = mid - 1
+print(mid + 1)
+
+
+##### The Solution
+import sys
+sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+lt = 0
+rt = n - 1
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if a[mid] == m:
+        print(mid + 1)
+        break
+
+    elif a[mid] > m :
+        rt = mid - 1
+    
+    else:
+        lt = mid + 1
