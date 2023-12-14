f25dcc45edfc7c4073c8815dde23719e8b085eb3
jongseok <jjs0615@naver.com>
2023-12-15 06:30:24 +0900

    rt ...

--- inflearn/algorithm/horses.py
+++ inflearn/algorithm/horses.py
@@ -5,27 +5,26 @@ n 개의 마구간과 c 마리의 말이 존재하는데,
 '''
 import sys
 sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
-def Count(len):
+def Count(length):
     cnt = 1
-    ep = stables[0]
+    start_p = stables[0]
     for i in range(1, n):
-        if stables[i] - ep >= len:
+        if stables[i] - start_p >= length:
             cnt += 1
-            ep = stables[i]
+            start_p = stables[i]
     return cnt
-
 
 n, c = map(int, input().split())
 stables = [int(input()) for _ in range(n)]
 stables.sort()
-lt = 1
+lt = stables[0]
 rt = stables[n-1]
 while lt <= rt:
-    mid = (lt + rt) // 2
-    if Count(mid) >= c:
+    mid = (lt + rt) // 2 # 둘 사이의 거리의 최대 (a + b) // 2
+    if Count(mid) >= c: # 말이 모자르게 마구간에 들어가는 게 문제일 뿐, 그 이외는 모두 허용
         res = mid
-        lt = mid + 1
+        lt = mid + 1 # 같거나 크면, 더 커져도 되므로 시작점을 옮김 
     else:
-        rt = mid - 1
+        rt = mid - 1 # 작으면 ...
 print(res)
             
