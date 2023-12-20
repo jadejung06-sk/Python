e26bc63003fdaa16a047e5778f1e716101831d6c
jongseok <jjs0615@naver.com>
2023-12-20 14:25:12 +0900

    decisional algorithm

--- inflearn/algorithm/horses.py
+++ inflearn/algorithm/horses.py
@@ -5,13 +5,14 @@ n 개의 마구간과 c 마리의 말이 존재하는데,
 '''
 import sys
 sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
+## 배치한 말의 수
 def Count(length):
     cnt = 1
     start_p = stables[0]
     for i in range(1, n):
         if stables[i] - start_p >= length:
             cnt += 1
-            start_p = stables[i]
+            start_p = stables[i] # 배치한 말의 마지막 지점
     return cnt
 
 n, c = map(int, input().split())
@@ -23,8 +24,8 @@ while lt <= rt:
     mid = (lt + rt) // 2 # 둘 사이의 거리의 최대 (a + b) // 2
     if Count(mid) >= c: # 말이 모자르게 마구간에 들어가는 게 문제일 뿐, 그 이외는 모두 허용
         res = mid
-        lt = mid + 1 # 같거나 크면, 더 커져도 되므로 시작점을 옮김 
+        lt = mid + 1 # 가장 가까우면서 더 먼 거리를 구하기 위해, 작게 나올 수 있는 부분을 제거함
     else:
-        rt = mid - 1 # 작으면 ...
+        rt = mid - 1 # 인정한 거리가 너무 멀어서, 배치가 어려운 경우..
 print(res)
             
