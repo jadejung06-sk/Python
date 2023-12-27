a2ef5232c3969eeebae9b7a3e8b5965c4f7569ff
jongseok <jjs0615@naver.com>
2023-12-27 22:18:15 +0900

    largest cnt

--- inflearn/algorithm/wrestling.py
+++ inflearn/algorithm/wrestling.py
@@ -2,23 +2,37 @@
 Greedy
 comparsion for heights and weights
 '''
+# import sys
+# sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
+# n = int(input())
+# people = [tuple(map(int, input().split())) for _ in range(n)]
+# people.sort()
+# # print(a)
+# cnt = 0
+# start_idx = 0
+
+# while start_idx < n:
+#     target_height = people[start_idx][0]
+#     target_weight = people[start_idx][1]
+#     for num, val in enumerate(people[start_idx:]): # h, w
+#         if num != 0:
+#             # print(target_height,  target_weight, val[0], val[1])
+#             if target_height < val[0] and target_weight < val[1]:
+#                 cnt += 1
+#                 break
+#     start_idx += 1
+# print(n - cnt)
+
+##### Solution
 import sys
 sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
 n = int(input())
 people = [tuple(map(int, input().split())) for _ in range(n)]
-people.sort()
-# print(a)
+people.sort(reverse = True)
+largest = 0
 cnt = 0
-start_idx = 0
-
-while start_idx < n:
-    target_height = people[start_idx][0]
-    target_weight = people[start_idx][1]
-    for num, val in enumerate(people[start_idx:]): # h, w
-        if num != 0:
-            # print(target_height,  target_weight, val[0], val[1])
-            if target_height < val[0] and target_weight < val[1]:
-                cnt += 1
-                break
-    start_idx += 1
-print(n - cnt)
+for x, y in people:
+    if y > largest:
+        largest = y
+        cnt += 1
+print(cnt)
