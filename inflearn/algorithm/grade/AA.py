20c85892befa0d675940390be4ae67714c4a8237
jongseok <jjs0615@naver.com>
2024-01-06 22:10:09 +0900

    stack - wrong answer now

--- inflearn/algorithm/grade/AA.py
+++ inflearn/algorithm/grade/AA.py
@@ -1,34 +1,15 @@
-'''
-Select a single number on the Right or the Left.
-The outputs are the largest number of numbers and Right / Left.
-'''
 import sys
-from collections import deque
-# sys.stdin = open(r"D:/2022/Python/inflearn/algorithm/grade/input.txt", 'r')
-n = int(input())
-a = list(map(int, input().split()))
-a = deque(a)
-last = 0
-lt = 0
-rt = n - 1
-res = ""
-tmp = []
-while lt <= rt:
-    if a[lt] > last:
-        tmp.append((a[lt], 'L'))
-    if a[rt] > last:
-        tmp.append((a[rt], 'R'))
-    tmp.sort()
-    if len(tmp) == 0:
-        break
-    else:
-        res += tmp[0][1]
-        last = tmp[0][0]
-        if tmp[0][1] == 'L':
-            lt += 1
-        else:
-            rt -= 1
-    tmp.clear()
-print(len(res))
-print(res)
-            
+# sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
+nums, m = map(int, input().split())
+nums = list(str(nums))
+m = len(nums) - m
+targets = set()
+for i in range(9, 0, -1):
+    answers = []
+    for n in nums:
+        targets.add(i)
+        if int(n) in targets:
+            answers.append(str(n))
+            if len(answers) == m:
+                break
+print(''.join(answers))
--- inflearn/algorithm/maximum.py
+++ inflearn/algorithm/maximum.py
@@ -1,7 +1,15 @@
 import sys
 sys.stdin = open(r'D:\2022\Python\inflearn\algorithm\grade\input.txt', 'r')
 nums, m = map(int, input().split())
-# print(len(str(nums)))
-length = len(str(nums)) - m
-# max(list(str(nums)))
-print(list(str(nums)).index('9'))
+nums = list(str(nums))
+m = len(nums) - m
+targets = set()
+for i in range(9, 0, -1):
+    answers = []
+    for n in nums:
+        targets.add(i)
+        if int(n) in targets:
+            answers.append(str(n))
+            if len(answers) == m:
+                break
+print(''.join(answers))
