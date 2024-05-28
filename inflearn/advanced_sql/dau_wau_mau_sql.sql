d7117b2be4eafe6baafe613cedf41e93d610f1c6
jongseok <jjs0615@naver.com>
2024-05-28 22:24:00 +0900

    stickiness

--- inflearn/advanced_sql/dau_wau_mau_sql.sql
+++ inflearn/advanced_sql/dau_wau_mau_sql.sql
@@ -57,3 +57,24 @@ join daily_wau b on a.curr_date = b.curr_date
 join daily_mau c on a.curr_date = c.curr_date;
 
 select * from daily_acquisitions;
+
+-- stickiness
+with temp_dau as (
+select :current_date as curr_date, count(distinct user_id) as dau
+from ga.ga_sess
+where visit_stime >= (:current_date - interval '1 days') and visit_stime < :current_date
+),
+temp_mau as (
+select :current_date as curr_date, count(distinct user_id) as mau
+from ga.ga_sess
+where visit_stime >= (:current_date - interval '30 days') and visit_stime < :current_date
+)
+select a.curr_date, a.dau, b.mau, round(100.0 * a.dau/b.mau, 2) as stickiness
+from temp_dau a
+join temp_mau b on a.curr_date = b.curr_date;
+
+-- 일주일 간 stickiness, 평균 stickiness
+select *, round(100.0 * dau/mau, 2) as stickiness
+	,round(avg(100.0 * dau/mau) over(), 2) as avg_stickiness
+from ga.daily_acquisitions
+where curr_date between to_date('2016-10-25', 'yyyy-mm-dd') and to_date('2016-10-31', 'yyyy-mm-dd')
