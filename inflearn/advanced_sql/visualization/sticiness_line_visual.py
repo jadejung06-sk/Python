72f6e54c375564013b884180dee8ec95f45bf51d
jongseok <jjs0615@naver.com>
2024-05-28 22:26:40 +0900

    postgres_engine

---
+++ inflearn/advanced_sql/visualization/sticiness_line_visual.py
@@ -0,0 +1,18 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+#  일주일 간 stickiness, 평균 stickiness
+sql = r'''select *, round(100.0 * dau/mau, 2) as stickiness
+	,round(avg(100.0 * dau/mau) over(), 2) as avg_stickiness
+from ga.daily_acquisitions
+where curr_date between to_date('2016-10-25', 'yyyy-mm-dd') and to_date('2016-10-31', 'yyyy-mm-dd')'''
+
+
+
+fig = make_subplots(specs = [[{"secondary_y": True}]])
+
+
+fig.update_layout(barmode = 'group', xaxis_tickangle = 45)
+
+fig.update_xaxes(type='')
+fig.show()
